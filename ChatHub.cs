beac2de9e3216e19651ce3ef5e863b698d68eade
wmaethner <32457351+wmaethner@users.noreply.github.com>
2017-12-15 13:32:23 -0600

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatApp
+{
+	public class ChatHub : Hub
+	{
+		static List<UserDetails> ConnectedUsers = new List<UserDetails>();
+		static List<MessageDetails> CurrentMessage = new List<MessageDetails>();
+
+		public void Connect(string userName)
+		{
+			string id = Context.ConnectionId;
+
+			if (ConnectedUsers.Count(x => x.ConnectedId == id) == 0)
+			{
+				ConnectedUsers.Add(new SignalRChatApp.UserDetails { ConnectedId = id, UserName = userName });
+
+				Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+
+				Clients.AllExcept(id).onNewUserConnected(id, userName);
+			}
+		}
+
+		public void SendMessageToAll(string userName, string message)
+		{
+			AddMessageInCache(userName, message);
+			Clients.All.messageReceived(userName, message);
+		}
+
+		public void SendPrivateMessage(string toUserId, string message)
+		{
+			string fromUserId = Context.ConnectionId;
+
+			var toUser = ConnectedUsers.FirstOrDefault(x => x.ConnectedId == toUserId);
+			var fromUser = ConnectedUsers.FirstOrDefault(x => x.ConnectedId == fromUserId);
+
+			if (toUser!=null && fromUser != null) 
+			{
+				Clients.Client(toUserId).sendPrivateMessage(fromUserId, fromUser.UserName, message);
+				Clients.Caller.sendPrivateMessage(toUserId, fromUser.UserName, message);
+			}
+		}
+
+		public override Task OnDisconnected(bool stopCalled)
+		{
+			var item = ConnectedUsers.FirstOrDefault(x => x.ConnectedId == Context.ConnectionId);
+			if (item != null)
+			{
+				ConnectedUsers.Remove(item);
+
+				var id = Context.ConnectionId;
+				Clients.All.onUserDisconnected(id, item.UserName);
+			}
+
+			return base.OnDisconnected(stopCalled);
+		}
+
+		private void AddMessageInCache(string userName, string message)
+		{
+			CurrentMessage.Add(new MessageDetails { UserName = userName, Message = message });
+		}
+	}
+}
---
+++ UserDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChatApp
+{
+	public class UserDetails
+	{
+		public string ConnectedId { get; set; }
+		public string UserName { get; set; }
+	}
+}
